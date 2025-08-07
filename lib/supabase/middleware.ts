32f1cac3240b35c940809de6d124f19e105f2313
pyodolski <pjwp0928w@gmail.com>
2025-08-07 20:42:27 +0900

    리다이렉션 추가

--- lib/supabase/middleware.ts
+++ lib/supabase/middleware.ts
@@ -9,9 +9,12 @@ export const createClient = (request: NextRequest) => {
     },
   })
 
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://okvrxpjincelvvwnvcts.supabase.co'
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdnJ4cGppbmNlbHZ2d252Y3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NTM1NzcsImV4cCI6MjA3MDEyOTU3N30.R4QwWmbd8tcuJQu3hU1yuxaGrh18khTg_J_ujo-9Szk'
+
   const supabase = createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl,
+    supabaseAnonKey,
     {
       cookies: {
         get(name: string) {
--- lib/supabase/server.ts
+++ lib/supabase/server.ts
@@ -3,10 +3,13 @@ import { cookies } from 'next/headers'
 
 export const createClient = () => {
   const cookieStore = cookies()
+  
+  const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://okvrxpjincelvvwnvcts.supabase.co'
+  const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9rdnJ4cGppbmNlbHZ2d252Y3RzIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NTQ1NTM1NzcsImV4cCI6MjA3MDEyOTU3N30.R4QwWmbd8tcuJQu3hU1yuxaGrh18khTg_J_ujo-9Szk'
 
   return createServerClient(
-    process.env.NEXT_PUBLIC_SUPABASE_URL!,
-    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    supabaseUrl,
+    supabaseAnonKey,
     {
       cookies: {
         get(name: string) {
